cc56a57fef3c740e5996f4e4e2f674ddfd33c0d7
Matthew <158129347+Montoose@users.noreply.github.com>
2024-08-26 17:47:23 -0500

    Add calculator functions
    Add functions for adding, multiplying, subtracting
    and dividing.

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    console.log(a + b);
+    return a + b;
+} 
+
+function subtract(a, b) {
+    console.log(a - b);
+    return a - b;
+}
+
+function multiply(a, b) {
+    console.log(a * b);
+    return a * b;
+}
+
+function divide(a, b) {
+    console.log(a / b);
+    return a / b;
+}
+
+add(5, 3);
+subtract(5, 3);
+multiply(5, 3);
+divide(5, 3);
